0a6884422d23c67538b33e28b15453af8f81785c
Ebuka John Onyejegbu <30580457+Johnng007@users.noreply.github.com>
2026-05-04 00:05:38 +0100

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-const url= 'http://localhost:5000/questions';
+const url= 'https://somto.onrender.com/questions';
 
 export const fetchQuestions =()=> axios.get(url);
 export const createQuestion =(newQuestion)=> axios.post(url,newQuestion);
 
-const url2= 'http://localhost:5000/results';
+const url2= 'https://somto.onrender.com/results';
 export const fetchResults =()=> axios.get(url2);
 export const createResult =(results)=> axios.post(url2,results);
